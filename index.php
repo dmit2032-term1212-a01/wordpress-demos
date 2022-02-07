d1d84eaac281d646d5d7fa89664aff7278e2430b
lauren17 <lauren.olsen17@gmail.com>
2022-02-06 18:48:50 -0700

    default/required fallback template

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * The main template file aka ultimate fallback
+ * 
+ * @package wp demos a01
+ * @since 1.0.0
+ */
+
+ //display header
+ get_header(); 
+?>
+
+<h1>Hello :)</h1>
+
+
+<?php get_footer(); ?>
