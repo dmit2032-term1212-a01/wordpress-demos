fa1d56cda7eafc2d604ea64adb8d804d73d0c11f
lauren17 <lauren.olsen17@gmail.com>
2022-02-08 11:23:19 -0700

    update archive template

--- archive.php
+++ archive.php
@@ -47,7 +47,7 @@
                         <?php wp_list_categories( 'depth=1' ); ?>
                     </ul>
                 </div>
-                <div class="col col-4">
+                <div class="col col-4 remove-default-widget-heading">
                     <!-- displaying all archives by month -->
                     <h2> <?php _e('Monthly Archives');?></h2>
                     <!-- display monthly archives as a list -->
