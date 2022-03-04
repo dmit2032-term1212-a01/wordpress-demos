77ee7699dbddc0dc905edb8d06d5282dd25b27dd
lauren17 <lauren.olsen17@gmail.com>
2022-03-04 11:30:51 -0700

    add custom logo

--- header.php
+++ header.php
@@ -14,4 +14,33 @@
         <?php wp_head(); ?>
     </head>
     <body <?php body_class(); ?> >
-        <header></header>
+        <header>
+            <div class="container">
+                <!-- option 2: if you don't logo/image it will display the site title (has a fallback ) -->
+                <div class="site-logo">
+                    <?php if ( ! has_custom_logo() ) { ?>
+                        <?php if ( is_front_page() && is_home() ) : ?>
+                            <!-- //if your page is set to front-page or blog display the site title (appearance > customize) -->
+                                <a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a>
+                        <?php else : ?>
+                            <!-- //if your page is not set to front-page or blog display the site title (appearance > customize) -->
+                            <a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a>
+                        <?php endif; ?>
+                            <!-- //otherwise display the custom logo. -->
+                    <?php } else {
+                        the_custom_logo();
+                    }?>
+                </div>
+                <!-- device specific (not the best method for overall responsiveness): wp_is_mobile() -->
+                
+                <!-- mobile screen only -->
+                <div class="sm-screens mobile">
+                    <a class="sm-logo" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" itemprop="url">
+                        <!-- properly displaying images hard-coded into templates -->
+                        <img src=" <?php bloginfo('template_directory');?>/assets/img/design+code-logo.svg " alt="<?php the_title(); ?>">
+                        <span class="sr-only"><?php bloginfo( 'name' ); ?></span>
+                    </a>
+                </div>
+            </div>
+            <!-- //container -->
+        </header>
