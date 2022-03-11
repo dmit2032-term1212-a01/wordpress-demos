a9629baad7c7cbdc8ae3b4922794be8d377dbd28
lauren17 <lauren.olsen17@gmail.com>
2022-03-11 11:54:24 -0700

    add footer widgets

--- footer.php
+++ footer.php
@@ -7,7 +7,23 @@
 ?>
 
 
-        <footer></footer>
+        <footer>
+            <div class="container">
+                <div class="row">
+                    <div class="col col-one">
+                        <?php if (is_active_sidebar('footer-col-one')) : ?>
+                            <?php dynamic_sidebar( 'footer-col-one' ); ?>
+                        <?php endif; ?>
+                    </div>
+                    <div class="col col-two">
+                        <?php if (is_active_sidebar('footer-col-two')) : ?>
+                            <?php dynamic_sidebar( 'footer-col-two' ); ?>
+                        <?php endif; ?>
+                    </div>
+                </div>
+                <p><?php _e('Copyright &copy;'); ?> <?php echo date('Y'); ?> <?php _e('WP Demos | For educational purposes')?></p>
+            </div>
+        </footer>
     <?php wp_footer(); ?>
     </body>
 </html>
