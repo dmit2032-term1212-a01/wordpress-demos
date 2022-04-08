c880c3eb2d66f3ec2a1d6f6d8dbe3f2d71411317
lauren17 <lauren.olsen17@gmail.com>
2022-04-07 21:07:46 -0600

    added pagination

--- single.php
+++ single.php
@@ -17,16 +17,23 @@
     </div>
     <div class="container-full">
         <div class="container two-thirds-layout">
-            <!--  //if there are posts then display them... -->
-            <?php if ( have_posts() ) : ?>
-                <?php while ( have_posts() ) : the_post();
-                    //template partial get's pulled into the loop
-                    get_template_part('template-parts/content', 'single');   
-                endwhile; ?>
-                <?php else :
-                    get_template_part('template-part/content', 'none');
-                ?>
-            <?php endif; ?>
+            <div class="two-thirds-col">
+                <!--  //if there are posts then display them... -->
+                <?php if ( have_posts() ) : ?>
+                    <?php while ( have_posts() ) : the_post();
+                        //template partial get's pulled into the loop
+                        get_template_part('template-parts/content', 'single');   
+                    endwhile; ?>
+                    <!-- pagination -->
+                    <ul class="pagination post">
+                        <li><?php previous_post_link('%link', '&larr; Previous');?> </li>
+                        <li><?php next_post_link('%link', 'Next &rarr;');?> </li>
+                    </ul>
+                    <?php else :
+                        get_template_part('template-part/content', 'none');
+                    ?>
+                <?php endif; ?>
+            </div>
             <div class="right-sidebar one-thrid-col">
                 <?php dynamic_sidebar( 'right-sidebar' ); ?>
             </div>
